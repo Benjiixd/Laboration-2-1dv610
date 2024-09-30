e904dfb771f5da4e6b1926c9d6be2da0a3b527ec
Benjamin Karlsson <smileben2012@gmail.com>
2024-09-30 12:57:36 +0200

    added saving one image

--- src/imageSaver/imageController.js
+++ src/imageSaver/imageController.js
@@ -16,8 +16,21 @@ class imageController {
    * @param image
    */
   async saveImage (image) {
-    await this.model.save(image)
-    return "Image saved"
+    if (!image) {
+      throw new Error('No image provided')
+    }
+    if (!image.mimetype) {
+        throw new Error('No mimetype provided')
+        }
+
+    const newImage = {
+        filename: image.originalName,
+        mimetype: image.mimetype,
+        size: image.size,
+        uploadedAt: new Date()
+    }
+    const saved = await this.model.save(newImage)
+    return saved
   }
 
   /**
