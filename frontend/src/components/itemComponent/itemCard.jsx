a3086a2f73597439b411af68176445165c23ae5c
Benjamin Karlsson <smileben2012@gmail.com>
2024-10-11 14:40:32 +0200

    tested itemCard

--- frontend/src/components/itemComponent/itemCard.jsx
+++ frontend/src/components/itemComponent/itemCard.jsx
@@ -7,20 +7,24 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import Image from "next/image";
 
 
 export default function ItemCard({ item }) {
     return (
         <Card>
             <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
+                <CardTitle>green hoodie</CardTitle>
+                <CardDescription>hoodie</CardDescription>
             </CardHeader>
             <CardContent>
-                <p>Card Content</p>
+                <Image src={'/hoodieImage.jpg'}
+                    width={500}
+                    height={500}
+                    alt="Picture of the author"></Image>
             </CardContent>
             <CardFooter>
-                <p>Card Footer</p>
+                <p>DIRTY</p>
             </CardFooter>
         </Card>
     );
